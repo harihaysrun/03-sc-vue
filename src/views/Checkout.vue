13aaba449ed52aaae6f71d9855c60998197a1af9
harihaysrun <nursyahirah98@outlook.com>
2022-03-23 07:32:38 +0000

    webhook not working

--- src/views/Checkout.vue
+++ src/views/Checkout.vue
@@ -7,8 +7,8 @@
 
 <script>
 
-// import axios from 'axios';
-// const BASE_API_URL = "https://nsy-03-sunscreen.herokuapp.com/api/";
+import axios from 'axios';
+const BASE_API_URL = "https://nsy-03-sunscreen.herokuapp.com/api/";
 
 export default{
   name: 'Checkout',
@@ -17,10 +17,10 @@ export default{
     this.user_id = localStorage.getItem("user_id"); 
 
     console.log(this.$route.params.id)
-    // let response = await axios.post(BASE_API_URL + 'success/' + this.$route.params.id, {
-    //   'user_id': this.user_id
-    // });
-    // console.log(response)
+    let response = await axios.post(BASE_API_URL + 'checkout/success/' + this.$route.params.id, {
+      'user_id': this.user_id
+    });
+    console.log(response)
   },
 }
 
